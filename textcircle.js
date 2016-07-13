a2c912fdbcf4a00e25c5c5d38323f62581b3d881
Ovidiu Bodea <antarexis@gmail.com>
2016-07-13 16:31:03 +0300

    New collection

--- textcircle.js
+++ textcircle.js
@@ -1,3 +1,4 @@
+this.Documents = new Mongo.Collection("documents");
 if (Meteor.isClient){
 
 }
@@ -5,5 +6,8 @@ if (Meteor.isClient){
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
+		if (!Documents.findOne()) {
+			Documents.insert({title: 'My new document'});
+		}
 	})
 }
