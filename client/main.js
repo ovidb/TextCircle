4b9a72b7d7eac02e446492f0f3bfd54ddaf37117
Ovidiu Bodea <antarexis@gmail.com>
2016-07-31 18:47:02 +0300

    Added iron:router

--- client/main.js
+++ client/main.js
@@ -1,6 +1,21 @@
 Meteor.subscribe("documents");
 Meteor.subscribe("editingUsers");
 
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function() {
+  this.render("navbar", {to:"header"});
+  this.render("docList", {to:"main"});
+});
+
+Router.route('/documents/:_id', function() {
+  Session.set("docid", this.params._id);
+  this.render("navbar", {to:"header"});
+  this.render("docItem", {to:"main"});
+});
+
 Template.docList.helpers({
   documents:function() {
     return Documents.find();
